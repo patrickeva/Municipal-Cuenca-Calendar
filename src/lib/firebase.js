7c5ca121e4f5ce3c572d8ea0efe49c0d7336685b
Patrick Eva <ptrkrmseva@gmail.com>
2026-05-14 17:32:48 +0800

    playsafe

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKD4NyjjVGI6aNQ6FsaBLfYMEU9iImTp0",
-  authDomain: "municipal-calendar-9f220.firebaseapp.com",
-  projectId: "municipal-calendar-9f220",
-  storageBucket: "municipal-calendar-9f220.firebasestorage.app",
-  messagingSenderId: "216374710835",
-  appId: "1:216374710835:web:67618a76719b166fa88946",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
